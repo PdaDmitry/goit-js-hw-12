10d67c8498ebddf8c2524669f13d8b7c95e5555e
PdaDmitry <146446934+PdaDmitry@users.noreply.github.com>
2024-04-19 20:04:11 +0000

    Deploying to gh-pages from @ PdaDmitry/goit-js-hw-12@d1b6302f02738234f9ef28f5a932721bf60bf421 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-var L=Object.defineProperty;var S=(s,e,t)=>e in s?L(s,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):s[e]=t;var d=(s,e,t)=>(S(s,typeof e!="symbol"?e+"":e,t),t),q=(s,e,t)=>{if(!e.has(s))throw TypeError("Cannot "+t)};var u=(s,e,t)=>(q(s,e,"read from private field"),t?t.call(s):e.get(s)),m=(s,e,t)=>{if(e.has(s))throw TypeError("Cannot add the same private member more than once");e instanceof WeakSet?e.add(s):e.set(s,t)};import{a as w,S as v,i as $}from"./assets/vendor-eded45c0.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const a of document.querySelectorAll('link[rel="modulepreload"]'))i(a);new MutationObserver(a=>{for(const r of a)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function t(a){const r={};return a.integrity&&(r.integrity=a.integrity),a.referrerPolicy&&(r.referrerPolicy=a.referrerPolicy),a.crossOrigin==="use-credentials"?r.credentials="include":a.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(a){if(a.ep)return;a.ep=!0;const r=t(a);fetch(a.href,r)}})();var l;class z{constructor(){m(this,l,15);d(this,"total_pages",1);this.KEY="42263617-81d7156b9f7b88cd7b1016c2a"}async getImages(e){const t=`https://pixabay.com/api/?key=${this.KEY}&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`;try{return(await w.get(t)).data}catch(i){throw new Error(i.response.status)}}get pageSize(){return u(this,l)}}l=new WeakMap;function E(){return`
+var b=(t,e,a)=>{if(!e.has(t))throw TypeError("Cannot "+a)};var c=(t,e,a)=>(b(t,e,"read from private field"),a?a.call(t):e.get(t)),u=(t,e,a)=>{if(e.has(t))throw TypeError("Cannot add the same private member more than once");e instanceof WeakSet?e.add(t):e.set(t,a)};import{a as L,S,i as w}from"./assets/vendor-eded45c0.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))o(s);new MutationObserver(s=>{for(const r of s)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function a(s){const r={};return s.integrity&&(r.integrity=s.integrity),s.referrerPolicy&&(r.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?r.credentials="include":s.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(s){if(s.ep)return;s.ep=!0;const r=a(s);fetch(s.href,r)}})();var n;class q{constructor(){u(this,n,15);this.KEY="42263617-81d7156b9f7b88cd7b1016c2a",this.URL="https://pixabay.com",this.resources="/api"}async getImages(e){const a={key:this.KEY,q:e,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:c(this,n)},o=`${this.URL}${this.resources}`;try{return(await L.get(o,{params:a})).data}catch(s){throw new Error(s.response.status)}}get pageSize(){return c(this,n)}}n=new WeakMap;function v(){return`
   <div class="container">
    <form class="form">
     <input class="input" type="text" name="picture" placeholder="Search images..." />
@@ -10,35 +10,35 @@ var L=Object.defineProperty;var S=(s,e,t)=>e in s?L(s,e,{enumerable:!0,configura
   <button class="btn-load btn-search" type="submit">Load more</button>
   <span class="loader second-load" data-loader>Loading images, please wait...</span>
   </div>
-  </div>`}function I(s){return s.map(t=>{const{webformatURL:i,largeImageURL:a,tags:r,likes:o,views:y,comments:h,downloads:b}=t;return`<li class="gallery-item">
-  <a class="gallery-link" href="${a}">
+  </div>`}function $(t){return t.map(a=>{const{webformatURL:o,largeImageURL:s,tags:r,likes:i,views:f,comments:y,downloads:h}=a;return`<li class="gallery-item">
+  <a class="gallery-link" href="${s}">
     <img
       class="gallery-image"
-      src="${i}"
-      data-source="${a}"
+      src="${o}"
+      data-source="${s}"
       alt=""
       title="${r}"
     />
     <ul class="image-info">
       <li class="info-item">
         <span class="info-item">Likes</span>
-        <span class="item-quantity">${o}</span>
+        <span class="item-quantity">${i}</span>
       </li>
        <li class="info-item">
         <span class="info-item">Views</span>
-        <span class="item-quantity">${y}</span>
+        <span class="item-quantity">${f}</span>
       </li>
        <li class="info-item">
         <span class="info-item">Comments</span>
-        <span class="item-quantity">${h}</span>
+        <span class="item-quantity">${y}</span>
       </li>
        <li class="info-item">
         <span class="info-item">Downloads</span>
-        <span class="item-quantity">${b}</span>
+        <span class="item-quantity">${h}</span>
       </li>
     </ul>
   </a>
   </li>`}).join(`
 
-`)}const M=E(),O=document.querySelector("body");O.insertAdjacentHTML("afterbegin",M);const T=document.querySelector("form"),g=document.querySelector(".loader");g.classList.add("is-hidden");const x=document.querySelector("[data-loader]");x.classList.add("is-hidden");const p=document.querySelector(".gallery");document.querySelector("input");document.querySelector("button");const n=document.querySelector(".btn-load");n.classList.add("is-hidden");const P=new z,H=new v(".gallery a");function f(s){return $.show({message:s,messageSize:"16px",messageWeight:"400",backgroundColor:"#ef4040",messageColor:"#fff",position:"topRight",iconUrl:"./img/error.svg"})}let c;T.addEventListener("submit",async s=>{if(s.preventDefault(),p.innerHTML="",c=s.target.elements.picture.value.trim(),!c){s.target.reset(),n.classList.add("is-hidden"),f("The form field must be filled in!");return}P.getImages(c).then(e=>{if(e.hits.length===0)n.classList.add("is-hidden"),f("Sorry, there are no images matching your search query. Please try again!");else{console.log(e.hits);const t=I(e.hits);p.innerHTML=t,n.classList.remove("is-hidden"),H.refresh()}}).catch(e=>{console.log(e)}).finally(()=>{g.classList.add("is-hidden"),s.target.reset()})});
+`)}const z=v(),E=document.querySelector("body");E.insertAdjacentHTML("afterbegin",z);const I=document.querySelector("form"),g=document.querySelector(".loader");g.classList.add("is-hidden");const M=document.querySelector("[data-loader]");M.classList.add("is-hidden");const m=document.querySelector(".gallery");document.querySelector("input");document.querySelector("button");const l=document.querySelector(".btn-load");l.classList.add("is-hidden");const O=new q,T=new S(".gallery a");function p(t){return w.show({message:t,messageSize:"16px",messageWeight:"400",backgroundColor:"#ef4040",messageColor:"#fff",position:"topRight",iconUrl:"./img/error.svg"})}let d;I.addEventListener("submit",async t=>{if(t.preventDefault(),m.innerHTML="",d=t.target.elements.picture.value.trim(),!d){t.target.reset(),l.classList.add("is-hidden"),p("The form field must be filled in!");return}try{const e=await O.getImages(d);if(console.log(e),e.hits.length===0)l.classList.add("is-hidden"),p("Sorry, there are no images matching your search query. Please try again!");else{console.log(e.hits);const a=$(e.hits);m.innerHTML=a,l.classList.remove("is-hidden"),T.refresh()}}catch(e){console.log(e)}finally{g.classList.add("is-hidden"),t.target.reset()}});
 //# sourceMappingURL=commonHelpers.js.map
